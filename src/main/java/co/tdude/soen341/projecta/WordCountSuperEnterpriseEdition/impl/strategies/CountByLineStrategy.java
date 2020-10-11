6f5de3d6d50393efd0515742ca4b98279eac74b5
Karim <karim.rhoualem@gmail.com>
2020-10-10 23:43:25 -0400

    Added javadocs to both the counting strategy interface and strategy implementations. #1

--- src/main/java/co/tdude/soen341/projecta/WordCountSuperEnterpriseEdition/impl/strategies/CountByLineStrategy.java
+++ src/main/java/co/tdude/soen341/projecta/WordCountSuperEnterpriseEdition/impl/strategies/CountByLineStrategy.java
@@ -4,8 +4,17 @@ import co.tdude.soen341.projecta.WordCountSuperEnterpriseEdition.interfaces.stra
 
 import java.util.logging.Logger;
 
+/**
+ * Implementation of the strategy design pattern that counts the total number of
+ * lines from a specified file.
+ */
 public class CountByLineStrategy implements WordCountCountStrategy {
 
+    /**
+     * Parses the fileContent parameter source file line by line and returns the final line count.
+     * @param fileContent The file source containing content to be counted.
+     * @return The total number of lines that have been counted in fileContent.
+     */
     @Override
     public int getCount(String fileContent) {
         int nLines = 0;
