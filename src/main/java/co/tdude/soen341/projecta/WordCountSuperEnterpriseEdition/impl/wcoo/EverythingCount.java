a0a72b83e6664b733a3bdbbfbb5222e89eb30107
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-10-14 20:23:37 -0400

    Added missing comment

--- src/main/java/co/tdude/soen341/projecta/WordCountSuperEnterpriseEdition/impl/wcoo/EverythingCount.java
+++ src/main/java/co/tdude/soen341/projecta/WordCountSuperEnterpriseEdition/impl/wcoo/EverythingCount.java
@@ -8,6 +8,14 @@ import co.tdude.soen341.projecta.WordCountSuperEnterpriseEdition.interfaces.stra
 import java.util.logging.Logger;
 
 public class EverythingCount extends EnterpriseWordCount {
+    /**
+     * The wc's constructor, which calls the parent constructor for the first counting strategy,
+     * then runs the two other startegies.
+     * @param countByCharacterStrategy The counting strategy
+     * @param args Array containing the command-line arguments
+     * @param banner The program's banner
+     * @param appName The program's name
+     */
     public EverythingCount(WordCountCountStrategy countByCharacterStrategy, String[] args, String banner, String appName) {
         super(countByCharacterStrategy, args, banner, appName);
         this.banner = false;
@@ -19,6 +27,10 @@ public class EverythingCount extends EnterpriseWordCount {
         Logger.getLogger("").info(String.valueOf(result));
     }
 
+    /**
+     * The wc's main method, specifying the count by line strategy, the banner and the program name.
+     * @param args Array containing the command-line arguments
+     */
     public static void main(String[] args) {
         EverythingCount ec = new EverythingCount(new CountByLineStrategy(), args, "wc Version 1.42b\nCopyright (C) ABC Inc 2020. All Rights Reserved.\nWritten by John Smith\n", "wc");
     }
